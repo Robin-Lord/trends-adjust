3b657d3fefb4d0f5bdec10f875b38b6a7d57753b
robin.lord <robin.lord@aira.net>
2024-09-14 02:58:02 +0000

    :ambulance: Apply hotfix - fix variable column names in charts causing errors

--- charting_helpers.py
+++ charting_helpers.py
@@ -1,7 +1,8 @@
-import plotly.graph_objects as go # type: ignore
+import plotly.graph_objects as go  # type: ignore
 import pandas as pd
 import numpy as np
 import streamlit as st
+
 
 def line_plot_highlighting_missing_sections(
         df: pd.DataFrame,
@@ -9,65 +10,91 @@ def line_plot_highlighting_missing_sections(
         date_col: str,
         target_col: str) -> go.Figure:
 
+    # Streamlit reruns can cause errors here - just standardise the df for visualisation
+    # It would be better to do this in a more standard way at a higher level but this is
+    # just a hotfix for now
+    df_to_use = df.copy()
+    future_df_to_use = future_df.copy()
+    renaming_dict = {}
+
+    if target_col not in df_to_use.columns:
+        renaming_dict["y"] = target_col
+    if date_col not in df_to_use.columns:
+        renaming_dict["ds"] = date_col
+    if renaming_dict != {}:
+        df_to_use.rename(renaming_dict, inplace=True)
+        future_df_to_use.rename(renaming_dict, inplace=True)
+
     # Create a line chart
     fig = go.Figure()
 
     # Add line trace
     fig.add_trace(go.Scatter(
-        x=df['ds'], 
-        y=df['y'], 
-        mode='lines', 
-        name='y')
-        )
-    
+        x=df_to_use[date_col],
+        y=df_to_use[target_col],
+        mode='lines',
+        name=target_col)
+    )
+
     # Find first missing row
-    first_missing = future_df["ds"].iloc[0]
-    last_missing = future_df["ds"].iloc[len(future_df)-1]
+    first_missing = future_df_to_use[date_col].iloc[0]
+    last_missing = future_df_to_use[date_col].iloc[len(future_df_to_use)-1]
 
     st.write(f"We will forecast data from {first_missing} to {last_missing}")
 
     fig.add_vrect(
-        x0 = first_missing,
-        x1 = last_missing,
-        fillcolor = "red",
-        opacity = 0.3,
-        line_width = 0
+        x0=first_missing,
+        x1=last_missing,
+        fillcolor="red",
+        opacity=0.3,
+        line_width=0
     )
 
     # Set the y-axis to logarithmic
     fig.update_layout(
-        yaxis_type="log", 
+        yaxis_type="log",
         title=f"Line Chart of {target_col} with Logarithmic Scale (area in red is missing data that will be forecast)",
-        xaxis_title=date_col, 
+        xaxis_title=date_col,
         yaxis_title=target_col
-        )
+    )
 
     # Return the figure
     return fig
-
 
 
 def visualise_forecast(
         df: pd.DataFrame,
         target_col: str,
         date_col: str):
-    
+
+    # Streamlit reruns can cause errors here - just standardise the df for visualisation
+    # It would be better to do this in a more standard way at a higher level but this is
+    # just a hotfix for now
+    df_to_use = df.copy()
+    renaming_dict = {}
+
+    if target_col not in df_to_use.columns:
+        renaming_dict["y"] = target_col
+    if date_col not in df_to_use.columns:
+        renaming_dict["ds"] = date_col
+    if renaming_dict != {}:
+        df_to_use.rename(renaming_dict, inplace=True)
+
     # Create a line chart
     fig = go.Figure()
 
     # Add line trace
     fig.add_trace(go.Scatter(
-        x=df['ds'], 
-        y=df['y'], 
-        mode='lines', 
+        x=df_to_use[date_col],
+        y=df_to_use[target_col],
+        mode='lines',
         name='historic')
-        )
-    
-    
-        # Add lower bound trace for the fan (invisible)
+    )
+
+    # Add lower bound trace for the fan (invisible)
     fig.add_trace(go.Scatter(
-        x=df['ds'], 
-        y=df['yhat_lower'],
+        x=df_to_use[date_col],
+        y=df_to_use['yhat_lower'],
         mode='lines',
         line=dict(width=0),
         showlegend=False)
@@ -75,8 +102,8 @@ def visualise_forecast(
 
     # Add upper bound trace for the fan and fill the area
     fig.add_trace(go.Scatter(
-        x=df['ds'], 
-        y=df['yhat_upper'],
+        x=df_to_use[date_col],
+        y=df_to_use['yhat_upper'],
         mode='lines',
         fill='tonexty',  # Fill area between yhat_upper and yhat_lower
         line=dict(width=0),
@@ -84,22 +111,19 @@ def visualise_forecast(
         showlegend=False)
     )
 
-    
     fig.add_trace(go.Scatter(
-        x=df['ds'], 
-        y=df['yhat'], 
-        mode='lines', 
+        x=df_to_use[date_col],
+        y=df_to_use['yhat'],
+        mode='lines',
         name='forecast')
-        )
-    
+    )
+
     # Set the y-axis to logarithmic
     fig.update_layout(
         title=f"Forecast of {target_col}",
-        xaxis_title=date_col, 
+        xaxis_title=date_col,
         yaxis_title=target_col
-        )
-    
-
+    )
 
     # Return the figure
     return fig
